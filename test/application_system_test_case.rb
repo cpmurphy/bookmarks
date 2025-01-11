5ffcc8264d755085f0e2dfce645ae174632d1bf0
Christian Murphy <cpmmu@yahoo.com>
2025-01-10 19:40:32 -0600

    Attempt to fix system tests
    Don't have these running locally because of chromedriver issues,
    but it's clear that we need to add support for the signed-in state.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,13 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+
+  def sign_in_as(user)
+    visit new_session_path
+    fill_in "email_address", with: user.email_address
+    fill_in "password", with: "password"
+    click_on "Sign in"
+    assert_text "Signed in successfully"
+  end
+
 end
--- test/system/bookmarks_test.rb
+++ test/system/bookmarks_test.rb
@@ -3,6 +3,8 @@ require "application_system_test_case"
 class BookmarksTest < ApplicationSystemTestCase
   setup do
     @bookmark = bookmarks(:one)
+    @private_bookmark = bookmarks(:private)
+    @user = users(:one)
   end
 
   test "visiting the index" do
@@ -10,38 +12,71 @@ class BookmarksTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Bookmarks"
   end
 
-  test "should create bookmark" do
+  test "should show public bookmarks when not signed in" do
+    visit bookmarks_url
+    assert_text @bookmark.title
+    assert_no_text @private_bookmark.title
+  end
+
+  test "should show all bookmarks when signed in" do
+    sign_in_as(@user)
+    visit bookmarks_url
+    assert_text @bookmark.title
+    assert_text @private_bookmark.title
+  end
+
+  test "should create bookmark when signed in" do
+    sign_in_as(@user)
     visit bookmarks_url
     click_on "New bookmark"
 
-    fill_in "Description", with: @bookmark.description
-    fill_in "Tags", with: @bookmark.tags
-    fill_in "Title", with: @bookmark.title
-    fill_in "Url", with: @bookmark.url
+    fill_in "Url", with: "http://example.com/new"
+    fill_in "Title", with: "New Bookmark"
+    fill_in "Description", with: "A new bookmark"
+    fill_in "Tags", with: "test,new"
     click_on "Create Bookmark"
 
     assert_text "Bookmark was successfully created"
-    click_on "Back"
   end
 
-  test "should update Bookmark" do
-    visit bookmark_url(@bookmark)
-    click_on "Edit this bookmark", match: :first
+  test "should update bookmark when signed in" do
+    sign_in_as(@user)
+    visit bookmarks_url
 
-    fill_in "Description", with: @bookmark.description
-    fill_in "Tags", with: @bookmark.tags
-    fill_in "Title", with: @bookmark.title
-    fill_in "Url", with: @bookmark.url
+    within("#bookmark_#{@bookmark.id}") do
+      click_on "Edit"
+    end
+
+    fill_in "Title", with: "Updated Title"
     click_on "Update Bookmark"
 
     assert_text "Bookmark was successfully updated"
-    click_on "Back"
+    assert_text "Updated Title"
   end
 
-  test "should destroy Bookmark" do
-    visit bookmark_url(@bookmark)
-    click_on "Destroy this bookmark", match: :first
+  test "should destroy bookmark when signed in" do
+    sign_in_as(@user)
+    visit bookmarks_url
+    page.accept_confirm do
+      within("#bookmark_#{@bookmark.id}") do
+        click_on "Delete"
+      end
+    end
 
     assert_text "Bookmark was successfully destroyed"
   end
+
+  test "should not show edit/delete buttons when not signed in" do
+    visit bookmarks_url
+    assert_no_text "Edit"
+    assert_no_text "Delete"
+    assert_no_text "New bookmark"
+  end
+
+  test "should redirect to sign in when trying to create bookmark" do
+    visit new_bookmark_url
+    assert_current_path new_session_path
+    assert_text "Please sign in to modify bookmarks"
+  end
+
 end
