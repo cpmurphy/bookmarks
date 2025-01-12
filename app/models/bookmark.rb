98b092945121addad6183dad5a72cecbe7ce6ea5
Christian Murphy <cpmmu@yahoo.com>
2025-01-12 15:16:18 -0600

    Show bookmarks in reverse order of creation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -6,6 +6,8 @@ class Bookmark < ApplicationRecord
 
   scope :public_only, -> { where(is_private: [ false, nil ]) }
 
+  default_scope { order(created_at: :desc) }
+
   def self.search(query)
     where("title LIKE ? OR description LIKE ? OR tags LIKE ?",
           "%#{query}%", "%#{query}%", "%#{query}%")
